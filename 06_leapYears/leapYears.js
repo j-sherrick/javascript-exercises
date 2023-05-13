519204c80ff8d0e2e0957a56873e5ee9557547ab
Jason Sherrick <jasonsherrick@gmail.com>
2023-05-13 00:39:32 -0500

    completes exercise 07

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isLeapYear = false;
+    if (year % 4 === 0)
+    {
+        if (year % 400 === 0)
+        {
+            isLeapYear = true;
+        }
+        else if (year % 100 !== 0)
+        {
+            isLeapYear = true;
+        }
+    }
+    return isLeapYear;
 };
 
 // Do not edit below this line
