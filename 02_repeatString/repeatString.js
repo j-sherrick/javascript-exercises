bdf9f4340ea0688c06f2b5ff8a430f0b847d3bed
Jason Sherrick <jasonsherrick@gmail.com>
2023-05-12 21:28:26 -0500

    completes exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, numTimes) {
+    if (numTimes < 0) return "ERROR";
 
+    let repeatedStr = "";
+    for (let i = 0; i < numTimes; i++) {
+        repeatedStr += str;
+    }
+    return repeatedStr;
 };
 
 // Do not edit below this line
