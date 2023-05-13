92d5c780bb4bcad998197e6e1e55638ae45bb366
Jason Sherrick <jasonsherrick@gmail.com>
2023-05-12 22:17:44 -0500

    completes exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    if (args.length === 0) return arr;
 
+    const newArr = [];
+    for (const item of arr)
+       if (!args.includes(item)) newArr.push(item);
+    return newArr;
 };
 
 // Do not edit below this line
